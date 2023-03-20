31d41589ed8c1c2a19f421b3fe5c024bb675d564
wevkleyton <kleytoncpd@gmail.com>
2023-03-20 15:23:28 -0300

    Voltando para o estado do paulo

--- app.js
+++ app.js
@@ -2,4 +2,3 @@ console.log("Rodando o sistema de gerenciamento de logs")
 console.log("aplicação está se conectando no banco de dados")
 // mais um console log de teste
 console.log("O Paulo fez esse commit sem você ver na aula")
-console.log("Enviando para o GitHub")
