1f3e268cc2a48f6cf1937f33870ee6457525fd80
kleyton <kleyton.wev@gmail.com>
2023-03-20 15:14:43 -0300

    Commit feito pelo paulo

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
 console.log("Rodando o sistema de gerenciamento de logs")
-console.log("aplicaç")
+console.log("aplicação está se conectando no banco de dados")
+// mais um console log de teste
+console.log("O Paulo fez esse commit sem você ver na aula")
